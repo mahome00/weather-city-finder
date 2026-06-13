16d522d839b7a674e627de6a52db731ac14bd898
mahome00 <j-community@proton.me>
2026-06-13 21:56:04 +0200

    CITY-1 Move city data to separate file

---
+++ src/data/cities.ts
@@ -0,0 +1,20 @@
+export const cities = [
+  {
+    name: "Stockholm",
+    country: "Sweden",
+    latitude: 59.3293,
+    longitude: 18.0686,
+  },
+  {
+    name: "Barcelona",
+    country: "Spain",
+    latitude: 41.3851,
+    longitude: 2.1734,
+  },
+  {
+    name: "Rome",
+    country: "Italy",
+    latitude: 41.9028,
+    longitude: 12.4964,
+  },
+];
